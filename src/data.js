23cb42105b564f3197264dd94c4d9b04d7331961
edasitagasi <a_b_c_d2004@mail.ru>
2023-08-30 12:43:23 +0300

    first commit

---
+++ src/data.js
@@ -0,0 +1,20 @@
+export const data = [
+
+    {
+        id: 1,
+        gift: "Parfume Chanel №5",
+        image: "https://images.unsplash.com/photo-1580238169310-f642ac65ed7d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3174&q=80"
+    },
+
+    {
+        id: 2,
+        gift: "Lipstick Kiko",
+        image: "https://images.unsplash.com/photo-1549427990-34b2f9d79b94?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2940&q=80"
+    },
+
+    {
+        id: 3,
+        gift: "Palette Chanel",
+        image: "https://images.unsplash.com/photo-1590156423742-3c58d6b9d605?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3174&q=80"
+    },
+]
